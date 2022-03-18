ab8468f9eae286d8a77c9fad54cdedc1aa4bbfb7
Allen McKenzie <allen.mcnichols@webdevstudios.com>
2022-03-17 21:28:53 -0700

    Adjust to use include

--- business-matchup.php
+++ business-matchup.php
@@ -52,7 +52,7 @@
 		$path = dirname(__FILE__) .  DIRECTORY_SEPARATOR . 'includes/classes' . DIRECTORY_SEPARATOR . $class . '.php';
 	 
 		if ( file_exists( $path ) ) {
-			require_once $path;
+			include $path;
 		}
 
 	}
